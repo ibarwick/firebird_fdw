b829aa66087ee39d1d5eec2d9ca9be2555aedb4b
Ian Barwick <barwick@gmail.com>
2018-08-01 23:16:55 +0900

    Don't report a null DETAIL field in error messages

--- src/connection.c
+++ src/connection.c
@@ -562,12 +562,24 @@ firebirdDbPath(char **address, char **database, int *port)
 void
 fbfdw_report_error(int errlevel, int pg_errcode, FBresult *res, FBconn *conn)
 {
+	char *primary_message = FQresultErrorField(res, FB_DIAG_MESSAGE_PRIMARY);
+	char *detail_message = FQresultErrorField(res, FB_DIAG_MESSAGE_DETAIL);
+
 	PG_TRY();
 	{
-		ereport(errlevel,
-				(errcode(pg_errcode),
-				 errmsg("%s", FQresultErrorField(res, FB_DIAG_MESSAGE_PRIMARY)),
-				 errdetail("%s", FQresultErrorField(res, FB_DIAG_MESSAGE_DETAIL))));
+		if (detail_message != NULL)
+		{
+			ereport(errlevel,
+					(errcode(pg_errcode),
+					 errmsg("%s", primary_message),
+  					 errdetail("%s", detail_message)));
+		}
+		else
+		{
+			ereport(errlevel,
+					(errcode(pg_errcode),
+					 errmsg("%s", primary_message)));
+		}
 	}
 	PG_CATCH();
 	{
