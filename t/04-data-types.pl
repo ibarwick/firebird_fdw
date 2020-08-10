f28fde698f24d219fc92eb10f3f6b3d5f845437a
Ian Barwick <barwick@gmail.com>
2020-08-10 22:21:05 +0900

    tests: check queries with boolean WHERE clause
    There's a subtle difference between PostgreSQL and Firebird (as of
    version 3.x): in Firebird, "IS NOT TRUE" and "IS NOT FALSE" do not
    cover rows where the boolean value is NULL, so we need to be sure
    queries return the same results as native PostgreSQL. This is important
    when pushing down boolean WHERE clauses.

--- t/04-data-types.pl
+++ t/04-data-types.pl
@@ -23,7 +23,7 @@ my $node = FirebirdFDWNode->new();
 # ----------------------
 
 if ($node->{firebird_major_version} >= 3) {
-	plan tests => 2;
+	plan tests => 8;
 }
 else {
 	plan tests => 1;
@@ -69,8 +69,10 @@ is (
 
 if ($node->{firebird_major_version} >= 3) {
 
-	# 2) Test BOOLEAN type
-	# ---------------------
+	# 2) Test BOOLEAN type (basic)
+	# ----------------------------
+    #
+    # Check that inserted values will round-trip
 
 	my $bool_insert_sql = sprintf(
 		<<EO_SQL,
@@ -93,6 +95,102 @@ EO_SQL
 		$q2_res,
 		qq/2|t\n3|f\n4|/,
 		q|Check BOOLEAN|,
+	);
+
+	# 3) Test BOOLEAN "IS TRUE"
+	# -------------------------
+
+    my $q3_sql = sprintf(
+        q|SELECT id, bool_type FROM %s WHERE id >= 2 AND bool_type IS TRUE|,
+        $table_name,
+    );
+
+    my $q3_res = $node->safe_psql($q3_sql);
+
+	is (
+		$q3_res,
+		qq/2|t/,
+		q|Check  BOOLEAN pushdowns with "IS TRUE"|,
+	);
+
+	# 4) Test BOOLEAN "IS NOT TRUE"
+	# -----------------------------
+
+    my $q4_sql = sprintf(
+        q|SELECT id, bool_type FROM %s WHERE id >= 2 AND bool_type IS NOT TRUE|,
+        $table_name,
+    );
+
+    my $q4_res = $node->safe_psql($q4_sql);
+
+	is (
+		$q4_res,
+		qq/3|f\n4|/,
+		q|Check BOOLEAN query with "IS NOT TRUE"|,
+	);
+
+	# 5) Test BOOLEAN "IS FALSE"
+	# --------------------------
+
+    my $q5_sql = sprintf(
+        q|SELECT id, bool_type FROM %s WHERE id >= 2 AND bool_type IS FALSE|,
+        $table_name,
+    );
+
+    my $q5_res = $node->safe_psql($q5_sql);
+
+	is (
+		$q5_res,
+		qq/3|f/,
+		q|Check BOOLEAN query with "IS FALSE"|,
+	);
+
+	# 6) Test BOOLEAN "IS NOT FALSE"
+	# ------------------------------
+
+    my $q6_sql = sprintf(
+        q|SELECT id, bool_type FROM %s WHERE id >= 2 AND bool_type IS NOT FALSE|,
+        $table_name,
+    );
+
+    my $q6_res = $node->safe_psql($q6_sql);
+
+	is (
+		$q6_res,
+		qq/2|t\n4|/,
+		q|Check BOOLEAN query with "IS NOT FALSE"|,
+	);
+
+	# 7) Test BOOLEAN "IS NULL"
+	# -------------------------
+
+    my $q7_sql = sprintf(
+        q|SELECT id, bool_type FROM %s WHERE id >= 2 AND bool_type IS NULL|,
+        $table_name,
+    );
+
+    my $q7_res = $node->safe_psql($q7_sql);
+
+	is (
+		$q7_res,
+		qq/4|/,
+		q|Check BOOLEAN query with "IS NULL"|,
+	);
+
+	# 8) Test BOOLEAN "IS NOT NULL"
+	# -----------------------------
+
+    my $q8_sql = sprintf(
+        q|SELECT id, bool_type FROM %s WHERE id >= 2 AND bool_type IS NOT NULL|,
+        $table_name,
+    );
+
+    my $q8_res = $node->safe_psql($q8_sql);
+
+	is (
+		$q8_res,
+		qq/2|t\n3|f/,
+		q|Check BOOLEAN query with "IS NOT NULL"|,
 	);
 }
 
