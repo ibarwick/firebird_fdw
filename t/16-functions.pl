4a91e4b7ae6eb964a6a1a2cd2e4f3ff21fac04ad
Ian Barwick <barwick@gmail.com>
2021-02-10 21:00:46 +0900

    Ensure non-superusers can execute firebird_version()
    The system catalogue table pg_user_mapping is restricted to superusers;
    use the corresponding view pg_user_mappings.
    
    Addresses GitHub #22.

---
+++ t/16-functions.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+# 16-functions.pl
+#
+# Checks for functions provided by firebird_fdw
+
+use strict;
+use warnings;
+
+use TestLib;
+use Test::More tests => 2;
+
+use FirebirdFDWNode;
+
+# Initialize nodes
+# ----------------
+
+my $node = FirebirdFDWNode->new();
+
+my ($version, $version_int) = $node->get_firebird_version();
+
+# 1. Check firebird_version() output
+# ----------------------------------
+
+my $version_expected = sprintf(
+    q/%s|%i|%s/,
+    $node->server_name(),
+    $version_int,
+    $version,
+);
+
+
+my $q1_sql = q|SELECT * FROM firebird_version()|;
+my $q1_res = $node->safe_psql($q1_sql);
+
+is (
+    $q1_res,
+    $version_expected,
+    q|Check firebird_version() output|,
+);
+
+
+
+# 2. Check firebird_version() output as non-superuser
+# ---------------------------------------------------
+
+$node->psql(q|CREATE USER foo|);
+
+my $q2_sql = q|SET session AUTHORIZATION foo; SELECT * FROM firebird_version()|;
+my $q2_res = $node->safe_psql($q2_sql);
+
+is (
+    $q2_res,
+    $version_expected,
+    q|Check firebird_version() output with non-superuser|,
+);
+
+
