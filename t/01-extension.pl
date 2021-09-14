1f359da715d968cbf849c5a0b16284ab35c6a232
Ian Barwick <barwick@gmail.com>
2021-09-14 23:31:58 +0900

    1.2.2 release

--- t/01-extension.pl
+++ t/01-extension.pl
@@ -27,7 +27,7 @@ my $node = FirebirdFDWNode->new();
 #
 # TODO: parse the value from "firebird_fdw.control" and check for a match
 
-my $version = '10201';
+my $version = '10202';
 
 my ($res, $res_stdout, $res_stderr) = $node->psql(q|SELECT firebird_fdw_version()|);
 
