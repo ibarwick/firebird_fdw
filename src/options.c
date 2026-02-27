ef92748bb7ac6dc46c7c73073168a65c1a44612d
Ian Barwick <barwick@gmail.com>
2026-02-27 20:01:48 +0900

    Update copyright to 2026

--- src/options.c
+++ src/options.c
@@ -2,7 +2,7 @@
  *
  * Helper functions to validate and parse the FDW options
  *
- * Copyright (c) 2013-2025 Ian Barwick
+ * Copyright (c) 2013-2026 Ian Barwick
  *
  * This software is released under the PostgreSQL Licence
  *
